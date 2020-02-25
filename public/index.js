b56ef1f38cd46050ed7405256d1fbe2a1a0ae1d2
pdossant <59825421+pdossant@users.noreply.github.com>
2020-02-25 16:30:50 +0100

    Add files via upload

--- public/index.js
+++ public/index.js
@@ -158,6 +158,187 @@ const actors = [{
   }]
 }];
 
+
+
 console.log(cars);
 console.log(rentals);
 console.log(actors);
+
+function dateDiff(date1, date2) {
+  var diff = {}                           // Initialisation du retour
+  var tmp = date2 - date1;
+
+  tmp = Math.floor(tmp / 1000);             // Nombre de secondes entre les 2 dates
+  diff.sec = tmp % 60;                    // Extraction du nombre de secondes
+  // console.log(diff.sec);
+  tmp = Math.floor((tmp - diff.sec) / 60);    // Nombre de minutes (partie entière)
+  diff.min = tmp % 60;                    // Extraction du nombre de minutes
+  // console.log(diff.min);
+  tmp = Math.floor((tmp - diff.min) / 60);    // Nombre d'heures (entières)
+  diff.hour = tmp % 24;                   // Extraction du nombre d'heures
+  // console.log(diff.hour);
+  tmp = Math.floor((tmp - diff.hour) / 24);   // Nombre de jours restants
+  diff.day = tmp;
+  //console.log(diff.day);
+
+  return diff;
+}
+
+// Step 1 -  Euro-Kilometers
+
+function rentalPrice() {
+  for (var rental of rentals) {
+    for (var car of cars) {
+      if (car.id == rental.carId) {
+        var distance = rental.distance * car.pricePerKm;
+        var date2 = new Date(rental.returnDate);
+        var date1 = new Date(rental.pickupDate);
+        var diff = dateDiff(date1, date2);
+        var days = diff.day;
+
+        if (days == 0) { days = 1; } //if the car is just for a 1day trip
+        //the diff will return 0 because same day
+
+        var time = days * cars.pricePerDay;
+        rental.price = (car.pricePerDay * days) + (car.pricePerKm * rental.distance);
+
+
+        console.log(car.name);
+        console.log("Rental price :"+rental.price+"$ \n\n");
+      }
+    }
+  }
+}
+rentalPrice();
+
+//Step 2 - Drive more, pay less
+
+function rentalPricePromotion()
+{
+  for (var rental of rentals) {
+    for (var car of cars) {
+      if (car.id == rental.carId) {
+        var distance = rental.distance * car.pricePerKm;
+        var date2 = new Date(rental.returnDate);
+        var date1 = new Date(rental.pickupDate);
+        var diff = dateDiff(date1, date2);
+        var days = diff.day;
+
+        if (days == 0) { days = 1; } //if the car is just for a 1day trip
+        //the diff will return 0 because same day
+        var time = days * car.pricePerDay;
+        rental.price = (car.pricePerDay * days) + (car.pricePerKm * rental.distance);
+
+        if(days>1 && days<5)
+        {
+          rental.price=rental.price*0.9;
+        }
+        if(days>4 && days<10)
+        {
+          rental.price=rental.price*0.7;
+        }
+        if(days>10)
+        {
+          rental.price=rental.price*0.5;
+        }
+
+        console.log(car.name);
+        console.log("Rental price :"+rental.price+"$ \n\n");
+      }
+    }
+  }
+}
+rentalPricePromotion()
+
+//Step 3 - Give me all your money
+
+function commission()
+{  
+  var commission = 0;
+  for(var rental of rentals)
+  {
+    if(rental.price > 0)
+    {
+      var date2 = new Date(rental.returnDate);
+      var date1 = new Date(rental.pickupDate);
+      var diff = dateDiff(date1, date2);
+      var days = diff.day;
+      if (days == 0) { days = 1; }
+
+      commission = rental.price*0.3;
+      rental.commission.insurance = commission * 0.5;
+      rental.commission.treasury = 1*days;
+      rental.commission.virtuo = commission - rental.commission.insurance - rental.commission.treasury;
+    }
+  console.log("Commission : "+commission);
+  console.log("insurance: "+rental.commission.insurance);
+  console.log("treasury: "+rental.commission.treasury);
+  console.log("virtuo: "+rental.commission.virtuo +"\n\n");
+
+  }
+  
+}
+commission()
+
+//Step 4 - The famous deductible
+
+function NewRentalPrice()
+{
+  for (var rental of rentals)
+  {
+    for (var car of cars)
+    {
+      if(car.id == rental.carId)
+      {
+        var date2 = new Date(rental.returnDate);
+        var date1 = new Date(rental.pickupDate);
+        var diff = dateDiff(date1, date2);
+        var days = diff.day;
+        if (days == 0) { days = 1; }
+
+        if(rental.options.deductibleReduction === true)
+        {
+          rental.price=((car.pricePerDay+4)*days)+(car.pricePerKm*rental.distance);
+        }
+      }
+    }
+    console.log(car.name);
+    console.log("Option : "+rental.options.deductibleReduction);
+    console.log("New Rental Price :"+rental.price+"$ \n\n");
+  }
+  
+}
+NewRentalPrice()
+
+//Step 5 - Pay the actors
+
+function payActors()
+{ 
+ for(var rental of rentals)
+ {
+  for(var actor of actors)
+  {
+    if(actor.rentalId == rental.id)
+    {
+      for(var pay of actor.payment)
+      {
+       if(pay.who == 'driver') 
+         pay.amount = rental.price;
+       if(pay.who == 'partner') 
+         pay.amount = rental.price*0.7;
+       if(pay.who == 'insurance') 
+         pay.amount = rental.commission.insurance;
+       if(pay.who == 'treasury') 
+         pay.amount = rental.commission.treasury;       
+       if(pay.who == 'virtuo') 
+         pay.amount = rental.commission.virtuo;
+     }     
+   }
+ }
+}
+console.log(actors);
+}
+
+payActors();
+
+
